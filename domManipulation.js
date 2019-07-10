3526858a7810e3cc9ad13ff38b1032e96600904d
Mackenson <mackensondorancy@yahoo.com>
2019-07-10 17:39:09 -0400

    DOM Manipulation with d3.js

---
+++ domManipulation.js
@@ -0,0 +1,27 @@
+
+d3.select('h1').style('color', 'red')
+.attr('class', 'heading')
+.text('Updated h1 tag');
+
+d3.select('body')
+.append('p')
+.text('This is paragraph 1');
+
+d3.select('body')
+.append('p')
+.text('This is paragraph 1');
+
+d3.select('body')
+.append('p')
+.text('This is paragraph 1');
+
+d3.select('body')
+.append('p')
+.text('This is paragraph 1');
+
+d3.select('body')
+.append('p')
+.text('This is paragraph 1');
+
+d3.selectAll('p')
+.style('color', 'blue');
